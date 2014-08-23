5e38a69b7bf821eb3f81ebcc945b12a05e57a0e0
tkmagesh <tkmagesh77@gmail.com>
2014-08-23 10:02:27 +0530

    Day - 1 - web server implementation

--- Day-1/04-webserver/server.js
+++ Day-1/04-webserver/server.js
@@ -1,11 +1,53 @@
-var http=require('http');
-var path=require('path');
+var http=require('http'),
+	path=require('path'),
+	fs = require('fs'),
+	url = require('url'),
+	calculator = require('./calculator'),
+	extensions = ['.html','.css','.js','.ico'];
+
+String.prototype.endsWith = function(extn){
+	return this.substr(this.length - extn.length) === extn;
+};
 
 function requestListener(req,res){
-	console.log("A new request is received for - ", req.url);
-	console.log("physical path = ", path.join(__dirname, req.url));
-	res.write("<h1>Welcome to Node</h1>");
-	res.end();
+	var urlObject = url.parse(req.url,true);
+	var fileName = path.join(__dirname, urlObject.pathname);
+	req.fileName = fileName;
+	req.urlObject = urlObject;
+
+	var isStaticResource = extensions.some(function(extn){
+		return fileName.endsWith(extn);
+	});
+	if (isStaticResource) {
+		processStaticResource(req,res);
+		return;
+	}
+	if (urlObject.pathname === '/calculator'){
+		var operation = urlObject.query.operation,
+			number1 = parseInt(urlObject.query.n1,10),
+			number2 = parseInt(urlObject.query.n2,10);
+
+		res.write(calculator[urlObject.query.operation](number1,number2).toString());
+		res.end();
+	}
+}
+
+function processStaticResource(req,res){
+	if (fs.existsSync(req.fileName)){
+		var readStream = fs.createReadStream(req.fileName,{encoding : "utf8"});
+		var readCount = 0;
+		readStream.on('data', function(data){
+			++readCount;
+			res.write(data);
+		});
+		readStream.on('end',function(){
+			console.log(readCount);
+			res.end();		
+		});
+	} else {
+		res.statusCode = 404;
+		res.end();	
+	}
 }
 var server = http.createServer(requestListener);
 server.listen(9090);
